00a433b39f08fc6ef59008a8c5bad57ff078028e
sheng-guang <604271396@qq.com>
2022-03-14 20:41:02 +0000

    Update login.js

--- project18/routes/login.js
+++ project18/routes/login.js
@@ -1,26 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-router.route("/login")
+router.route("/")
     .post(function (req,res){
         let password=req.body.password;
         let username=req.body.userName;
 
+        res.setHeader('Content-Type', 'application/json');
+
         if(password!='123'||username!='123'){
-            res.setHeader('Content-Type', 'application/json');
             res.json('error');
         }
-        res.setHeader('Content-Type', 'application/json');
         res.json('login success');
     });
 
-router.route("/register")
-    .post(function (req,res){
-        let password=req.body.password;
-        let username=req.body.userName;
 
-
-    });
 
 
 module.exports = router;
