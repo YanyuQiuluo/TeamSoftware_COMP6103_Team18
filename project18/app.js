0154edbc09362f650d3df5890f8331d84985f694
sheng-guang <604271396@qq.com>
2022-03-15 11:39:27 +0000

    Update app.js

--- project18/app.js
+++ project18/app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var registerRouter = require('./routes/register');
-var accountRouter = require('./routes/Account');
+var loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -23,8 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/Register', registerRouter);
-app.use('/Register', accountRouter);
+app.use('/register', registerRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
