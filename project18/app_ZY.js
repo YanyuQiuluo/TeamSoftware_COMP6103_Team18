ed831ce59a983892e93063afa3c5cbb89e0da8e7
YanyuQiuluo <73321893+YanyuQiuluo@users.noreply.github.com>
2022-03-21 18:51:19 +0000

    countrylist

--- project18/app_ZY.js
+++ project18/app_ZY.js
@@ -1,7 +1,9 @@
 const registerRouter = require("./routes/register");
+const countrylistRouter = require("./routes/countrylist");
 
 function Load(app) {
     app.use('/register', registerRouter);
+    app.use('/countrylist', countrylistRouter)
 
 }
 module.exports=Load
