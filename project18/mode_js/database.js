c9bd850239fd0ef5412dd84e214f09d027bd273b
sheng-guang <604271396@qq.com>
2022-03-22 12:36:09 +0000

    database

---
+++ project18/mode_js/database.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+let a=0;
+a=a+1;
+console.log("load DB"+a);
+// First you need to create a connection to the database
+// Be sure to replace 'user' and 'password' with the correct values
+const connection = mysql.createConnection({
+    host: 'localhost',
+    database: 'new_schema',
+    user: 'root',
+    password: '234567891'
+
+});
+
+module.exports = connection;
