a2fbae74db20f8572bb7862ba1b87026aa6d0292
YanyuQiuluo <73321893+YanyuQiuluo@users.noreply.github.com>
2022-03-22 11:39:24 +0000

    Update app_ZY.js

--- project18/app_ZY.js
+++ project18/app_ZY.js
@@ -1,11 +1,9 @@
 const registerRouter = require("./routes/register");
 const countrylistRouter = require("./routes/countrylist");
-var connection = require('./routes/database');
 
 function Load(app) {
     app.use('/register', registerRouter);
     app.use('/countrylist', countrylistRouter);
-    app.use('/database', connection);
 
 }
 module.exports=Load
