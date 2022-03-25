146ceaa0beb244b9aa5f61cbc245c1c9bbc6c2f9
Yue Zhou <yzhou209@sheffield.ac.uk>
2022-03-25 20:15:58 +0000

    verify code

--- project18/mode_js/automailer.js
+++ project18/mode_js/automailer.js
@@ -13,11 +13,33 @@ const config = {
 const server = automailer.createTransport(config);
 
 // Send email
-module.exports = function (mail){
+module.exports = function (rec_address){
+
+    var mail = {
+        // sender
+        from: 'SunlightCommunity18@gmail.com',
+        // subject
+        subject: 'Verification code',
+        // receiver
+        to:rec_address,//comes from front end
+        // content of the email;
+        text: veri_code+' is your verification code.'
+    };
+
     server.sendMail(mail, function(error, info){
         if(error) {
             return console.log(error);
         }
         console.log('mail sent:', info.response);
     });
+
+    veri_code(){
+        var Num="";
+        for(var i=0;i<6;i++)
+        {
+            Num+=Math.floor(Math.random()*10);
+        }
+        return Num;
+    }
+
 };
