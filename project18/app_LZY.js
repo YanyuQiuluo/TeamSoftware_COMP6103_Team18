adb93e5146790fc5135f6db46ab460b96c86bde5
Li Zeyu <zli259@sheffield.ac.uk>
2022-03-25 19:26:56 +0000

    country list

--- project18/app_LZY.js
+++ project18/app_LZY.js
@@ -1,8 +1,13 @@
 const indexRouter = require("./routes/index");
 var usersRouter = require('./routes/users');
+const registerRouter = require("./routes/registerPage");
+const countrylistRouter = require("./routes/countrylistPage")
+
 function Load(app) {
     app.use('/', indexRouter);
     app.use('/users', usersRouter);
     app.use('/loginPage', require('./routes/loginPage'));
+    app.use('/countrylistPage', countrylistRouter);
+
 }
 module.exports=Load
