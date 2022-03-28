a37eef282fafbfc85e1ccf2ee99e16ba1606be81
sheng-guang <604271396@qq.com>
2022-03-28 19:12:46 +0100

    del test routeslist

--- project18/routes/login.js
+++ project18/routes/login.js
@@ -8,37 +8,29 @@ router.route("/")
     .post(function (req,res){
 
         res.setHeader('Content-Type', 'application/json');
-        try {
-            let password=req.body.password;
-            let username=req.body.userName;
-            let conn=require('../mode_js/database');
-            // let sql="SELECT * FROM user";
-            let sql="select * from user where username=  '"+username+"';";
-            console.log(sql);
-            // let sql="select * from user where username == "+username;
 
-            conn.query(sql,
-                function (err,re){
-                    if(err)res.json(err);
-                    if(Array.isArray( re)==false||re.length==0){
-                        res.json( result.fail("account no found"));
-                        return;
-                    }
-                    if(re[0].password!=password){
-                        res.json( result.fail("password error"));
-                        return;
-                    }
-                    token =Tokens.onLogIn(re[0]);
-                    res.json(result.success(token));
-                });
-        }
-        catch (e){
-            res.json(e);
-        }
+        let password=req.body.password;
+        let username=req.body.userName;
+        let conn=require('../mode_js/database');
+        // let sql="SELECT * FROM user";
+        let sql="select * from user where username=  '"+username+"';";
+        console.log(sql);
+        // let sql="select * from user where username == "+username;
 
-
-
-
+        conn.query(sql,
+            function (err,re){
+                if(err)res.json(err);
+                if(Array.isArray( re)==false||re.length==0){
+                    res.json( result.fail("account no found"));
+                    return;
+                }
+                if(re[0].password!=password){
+                    res.json( result.fail("password error"));
+                    return;
+                }
+                token =Tokens.onLogIn(re[0]);
+                res.json(result.success(token));
+            });
     });
 
 
