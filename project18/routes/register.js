d3fd0a37955ab8e46289185d570a78d4056d1a0f
Yue Zhou <yzhou209@sheffield.ac.uk>
2022-03-28 19:23:06 +0100

    Update register.js

--- project18/routes/register.js
+++ project18/routes/register.js
@@ -1,12 +1,23 @@
 var express = require('express');
+const connection = require("../mode_js/database");
+const result = require("../mode_js/result");
 var router = express.Router();
 
-router.route("/")
+router.route("/register")
     .post(function (req,res){
+
+        let connection = require('../mode_js/database');
 
         let username=req.body.userName;
         let password=req.body.password;
         let email=req.body.email;
+
+
+        let sql = "SELECT * FROM country";
+        connection.query(sql,function (err,results){
+            if(err) throw err;
+            res.json(result.success( results));
+        });
 
         if(!username || !password || !email){
             res.setHeader('Content-Type', 'application/json');
@@ -14,6 +25,15 @@ router.route("/")
         }
         res.setHeader('Content-Type', 'application/json');
         res.json('Register success');
+
+    });
+
+router.route("/verify")
+    .post(function (req,res){
+
+        let username=req.body.userName;
+        let password=req.body.password;
+        let email=req.body.email;
 
     });
 
