0528ab740835cd167eb02a4264e9e7de0dc24ca8
sheng-guang <604271396@qq.com>
2022-03-29 12:40:38 +0100

    aa

--- project18/routes/countrylist.js
+++ project18/routes/countrylist.js
@@ -1,6 +1,5 @@
 var express = require('express');
 const result = require("../mode_js/result");
-const connection = require("../database");
 var router = express.Router();
 
 
@@ -24,22 +23,9 @@ router.route("/")
         let carbon_emission=req.body.carbon_emission;
         let price_of_solar_panel=req.body.price_of_solar_panel;
         let savings=req.body.savings;
-=======
-
-
-
-
-                let country_id=req.body.country_id;
-                let country_name=req.body.country_name;
-                let gdp=req.body.gdp;
-                let carbon_emission=req.body.carbon_emission;
-                let price_of_solar_panel=req.body.price_of_solar_panel;
-                let savings=req.body.savings;
->>>>>>> Stashed changes
 
         res.setHeader('Content-Type', 'application/json');
 
-<<<<<<< Updated upstream
         if(!country_id || !country_name || !gdp || !carbon_emission || !price_of_solar_panel
             || !savings){
                 res.json('upload failed');
@@ -48,14 +34,6 @@ router.route("/")
         res.json({'country_id':country_id, 'country_name':country_name,
                 'gdp':gdp, 'carbon_emission':carbon_emission,
                 'price_of_solar_panel':price_of_solar_panel,'savings':savings});*/
-                if(!country_id || !country_name || !gdp || !carbon_emission || !price_of_solar_panel
-                    || !savings){
-                        res.json('upload failed!');
-                }
-                res.setHeader('Content-Type', 'application/json');
-                res.json({'country_id':country_id, 'country_name':country_name,
-                        'gdp':gdp, 'carbon_emission':carbon_emission,
-                        'price_of_solar_panel':price_of_solar_panel,'savings':savings});
 
     });
 
