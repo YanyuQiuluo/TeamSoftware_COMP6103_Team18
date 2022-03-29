ddeaa7dda29e8757a659acb53a95c874da457083
sheng-guang <604271396@qq.com>
2022-03-29 12:06:13 +0100

    Revert "aa"
    This reverts commit 95543f23dcbf7092c5dfb2508844bf77df5e1c6a.

--- project18/routes/login.js
+++ project18/routes/login.js
@@ -21,37 +21,26 @@ router.route("/")
         if(!password){
             res.json(result.fail("password empty"));
         }
-        require('../mode_js/MongoDB').table("user",db={})
-            .then(x=>x.findOne({"username":username}).toArray())
-            .then(x=>{
-                if(x.length==0) throw "account no found"
-                if(x[0].password!=password)throw "password error"
+
+        let sql="select * from user where username=  '"+username+"';";
+        console.log("login  "+sql);
+        // let sql="select * from user where username == "+username;
+        conn.query("")
+            .then()
+        conn.query(sql,
+            function (err,re){
+                if(err)res.json(err);
+                if(Array.isArray( re)==false||re.length==0){
+                    res.json( result.fail("account no found"));
+                    return;
+                }
+                if(re[0].password!=password){
+                    res.json( result.fail("password error"));
+                    return;
+                }
                 token =Tokens.onLogIn(re[0]);
                 res.json(result.success(token));
-            })
-
-            .catch(x=>res.json(result.fail( x)))
-            .finally(()=>{if(db.db){console.log("here"); db.db.close();}})
-
-        // let sql="select * from user where username=  '"+username+"';";
-        // console.log("login  "+sql);
-        // // let sql="select * from user where username == "+username;
-        // conn.query("")
-        //     .then()
-        // conn.query(sql,
-        //     function (err,re){
-        //         if(err)res.json(err);
-        //         if(Array.isArray( re)==false||re.length==0){
-        //             res.json( result.fail("account no found"));
-        //             return;
-        //         }
-        //         if(re[0].password!=password){
-        //             res.json( result.fail("password error"));
-        //             return;
-        //         }
-        //         token =Tokens.onLogIn(re[0]);
-        //         res.json(result.success(token));
-        //     });
+            });
     });
 
 
