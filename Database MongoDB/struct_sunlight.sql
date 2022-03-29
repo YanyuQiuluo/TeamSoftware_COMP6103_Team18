4368413998c09840513cb263737b24f8244a9204
sheng-guang <604271396@qq.com>
2022-03-29 11:37:06 +0100

    db data

--- Database MongoDB/struct_sunlight.sql
+++ Database MongoDB/struct_sunlight.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50006
  File Encoding         : 65001
 
- Date: 29/03/2022 11:19:29
+ Date: 29/03/2022 11:12:55
 */
 
 
@@ -22,35 +22,10 @@ db.getCollection("country").drop();
 db.createCollection("country");
 
 // ----------------------------
-// Documents of country
-// ----------------------------
-db.getCollection("country").insert([ {
-    _id: ObjectId("6242dcee9c6f0000d400186a"),
-    "country_id": "-",
-    "country_name": "-",
-    gdp: "-",
-    "carbon_emissions": "-",
-    "price_of_solar_panel": "-",
-    savings: "-",
-    "country_image": "-"
-} ]);
-
-// ----------------------------
 // Collection structure for household
 // ----------------------------
 db.getCollection("household").drop();
 db.createCollection("household");
-
-// ----------------------------
-// Documents of household
-// ----------------------------
-db.getCollection("household").insert([ {
-    _id: ObjectId("6242dcf39c6f0000d400186b"),
-    savings: "-",
-    "household_id": "-",
-    "feul_usage_pm": "-",
-    "electicity_usage_pm": "-"
-} ]);
 
 // ----------------------------
 // Collection structure for qqq.chunks
@@ -66,10 +41,6 @@ db.getCollection("qqq.chunks").createIndex({
 });
 
 // ----------------------------
-// Documents of qqq.chunks
-// ----------------------------
-
-// ----------------------------
 // Collection structure for qqq.files
 // ----------------------------
 db.getCollection("qqq.files").drop();
@@ -82,46 +53,13 @@ db.getCollection("qqq.files").createIndex({
 });
 
 // ----------------------------
-// Documents of qqq.files
-// ----------------------------
-
-// ----------------------------
 // Collection structure for transaction
 // ----------------------------
 db.getCollection("transaction").drop();
 db.createCollection("transaction");
 
 // ----------------------------
-// Documents of transaction
-// ----------------------------
-db.getCollection("transaction").insert([ {
-    _id: ObjectId("6242dcf79c6f0000d400186c"),
-    dataTime: "-",
-    amount: "-",
-    "country_id": "-",
-    "user_id": "-"
-} ]);
-
-// ----------------------------
 // Collection structure for user
 // ----------------------------
 db.getCollection("user").drop();
 db.createCollection("user");
-
-// ----------------------------
-// Documents of user
-// ----------------------------
-db.getCollection("user").insert([ {
-    _id: ObjectId("6242dcfc9c6f0000d400186d"),
-    "user_id": "-",
-    "user_full_name": "-",
-    username: "-",
-    password: "-",
-    telephone: "-",
-    "address_first_line": "-",
-    "street_name": "-",
-    city: "-",
-    country: "-",
-    postcode: "-",
-    userType: "-"
-} ]);
