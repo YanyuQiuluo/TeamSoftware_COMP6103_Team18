5ff140eef405a8dedfd40bf1fee88e115eb0b02f
Li Zeyu <zli259@sheffield.ac.uk>
2022-03-30 16:06:35 +0100

    create "contact page"

--- project18/app_LZY.js
+++ project18/app_LZY.js
@@ -2,12 +2,14 @@ const indexRouter = require("./routes/index");
 var usersRouter = require('./routes/users');
 const registerRouter = require("./routes/registerPage");
 const countrylistRouter = require("./routes/countrylistPage")
+const contactRouter = require("./routes/contactPage")
 
 function Load(app) {
     app.use('/', indexRouter);
     app.use('/users', usersRouter);
     app.use('/loginPage', require('./routes/loginPage'));
     app.use('/countrylistPage', countrylistRouter);
+    app.use('/contactPage',contactRouter);
 
 }
 module.exports=Load
