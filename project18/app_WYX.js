465391a8e3679e13dbbe64ca867d91032e287131
Yixiang Wang <ywang597@sheffield.ac.uk>
2022-04-25 16:18:35 +0100

    country detail

--- project18/app_WYX.js
+++ project18/app_WYX.js
@@ -1,7 +1,9 @@
 
 var loginRouter = require('./routes/login');
+var countryDetail=require('./routes/CountryDetail')
 function Load(app) {
     app.use('/login', loginRouter);
+    app.use("/country_detail",countryDetail);
 }
 
 module.exports=Load
