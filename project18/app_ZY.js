a9d855f8ba7fc5477a86be57584ea7246f342570
Yue Zhou <yzhou209@sheffield.ac.uk>
2022-04-25 17:27:21 +0100

    1

--- project18/app_ZY.js
+++ project18/app_ZY.js
@@ -1,29 +1,15 @@
 const registerRouter = require("./routes/register");
 const verifyRouter = require("./routes/register");
 const countrylistRouter = require("./routes/countrylist");
+const userPageRouter = require("./routes/userPage");
 // const rankedcountrylistRouter = require("./routes/rankedcountrylist");
-const automailer = require("./mode_js/automailer");
+// const automailer = require("./mode_js/automailer");
 
 
 function Load(app) {
     app.use('/register', registerRouter);
     app.use('/register', verifyRouter);
     app.use('/countrylist', countrylistRouter);
-    // app.use('/rankedcountrylist', rankedcountrylistRouter);
-    //const automailer = require("./mode_js/automailer");
-
-    /*var mail = {
-        // sender
-        from: 'SunlightCommunity18@gmail.com',
-        // subject
-        subject: 'Welcome to SunlightCommunity',
-        // receiver
-        to:'604271396@qq.com',//comes from front end
-        // content of the email;
-        text: 'This message comes from nodejs.'
-    };*/
-
-    //automailer(mail);
-
+    app.use('/userPage', userPageRouter);
 }
 module.exports=Load
