22829f70b21e3ea6070921cbb2248aa8a9339250
Yixiang Wang <ywang597@sheffield.ac.uk>
2022-04-25 19:05:23 +0100

    id secret

--- project18/routes/PayPalCfg.js
+++ project18/routes/PayPalCfg.js
@@ -2,7 +2,7 @@ const paypal = require('paypal-rest-sdk');
 
 paypal.configure({
     'mode': 'sandbox', //sandbox or live
-    'client_id': '####yourclientid######',
-    'client_secret': '####yourclientsecret#####'
+    'client_id': 'AZy5f1Hb6e31rRiN1Yl7C5Rl0O2aDZC7aK-QpEGEjDumOPORvwOPpwMC-6wewOBTGI_TxGvMrje-Zu2-',
+    'client_secret': 'EFbe8sLzq2XgLlnhILj5FNIvsHlUAhVbCVbxHLCelZ6tobaW_rWpY7DYbAJWP2xFwqGC1JSPwD9LqwCD'
 });
 module.exports=paypal;
