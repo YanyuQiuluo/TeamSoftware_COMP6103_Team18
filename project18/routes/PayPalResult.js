ab4f11491322df06c7a5f2a5a9b1c9915551086f
Yixiang Wang <ywang597@sheffield.ac.uk>
2022-04-25 19:00:42 +0100

    paypal cfg

---
+++ project18/routes/PayPalResult.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const result = require("../mode_js/result");
+const Tokens = require("../mode_js/Tokens");
+require("PayPalCfg");
+
+router.route("/")
+    .post(function (req,res){
+
+    });
+
+module.exports=router;
+
+
