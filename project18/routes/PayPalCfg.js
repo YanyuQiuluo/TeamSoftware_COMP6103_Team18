9ea61443cfc634d20c02a3601dfadd4024bf8071
Yixiang Wang <ywang597@sheffield.ac.uk>
2022-04-25 19:00:42 +0100

    paypal cfg

---
+++ project18/routes/PayPalCfg.js
@@ -0,0 +1,8 @@
+const paypal = require('paypal-rest-sdk');
+
+paypal.configure({
+    'mode': 'sandbox', //sandbox or live
+    'client_id': '####yourclientid######',
+    'client_secret': '####yourclientsecret#####'
+});
+module.exports=paypal;
