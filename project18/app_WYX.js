1f7a9e0ffee13abf31f64d3edbfec2aad5986a8a
Yixiang Wang <ywang597@sheffield.ac.uk>
2022-05-03 10:59:13 +0100

    success cancel

--- project18/app_WYX.js
+++ project18/app_WYX.js
@@ -1,9 +1,13 @@
 
 var loginRouter = require('./routes/login');
-var countryDetail=require('./routes/CountryDetail')
+var countryDetail=require('./routes/CountryDetail');
+var success=require('./routes/PayPalResultSuccess');
+var cancel=require('./routes/PayPalResultCancel');
 function Load(app) {
     app.use('/login', loginRouter);
     app.use("/country_detail",countryDetail);
+    app.use("/success",success);
+    app.use("/cancel",cancel);
 }
 
 module.exports=Load
--- project18/routes/PayPalResultCancel.js
+++ project18/routes/PayPalResultCancel.js
@@ -2,13 +2,11 @@ const express = require('express');
 const router = express.Router();
 const result = require("../mode_js/result");
 const Tokens = require("../mode_js/Tokens");
-require("PayPalCfg");
+const paypal = require("PayPalCfg");
 
 router.route("/")
     .post(function (req,res){
-
+        res.end("payment cancel");
     });
 
 module.exports=router;
-
-
