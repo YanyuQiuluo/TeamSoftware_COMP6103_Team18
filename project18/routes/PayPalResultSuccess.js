ca26022581b111a7a711ba68f2a129833e550e8f
Yixiang Wang <ywang597@sheffield.ac.uk>
2022-05-03 11:08:10 +0100

    upd success

--- project18/routes/PayPalResultSuccess.js
+++ project18/routes/PayPalResultSuccess.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = express.Router();
 const result = require("../mode_js/result");
 const Tokens = require("../mode_js/Tokens");
-const paypal = require("PayPalCfg");
-
+const paypal = require("./PayPalCfg");
 router.route("/")
     .post(function (req,res){
 
