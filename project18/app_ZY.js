3db25f5837bd4ff50c72df1071eab39dc634f148
Yue Zhou <yzhou209@sheffield.ac.uk>
2022-05-03 11:15:42 +0100

    paypal_start

--- project18/app_ZY.js
+++ project18/app_ZY.js
@@ -12,22 +12,7 @@ function Load(app) {
     app.use('/register', verifyRouter);
     app.use('/countrylist', countrylistRouter);
     app.use('/userPage', userPageRouter);
-    app.use('/paypal', paypalRouter);
-    // app.use('/rankedcountrylist', rankedcountrylistRouter);
-    //const automailer = require("./mode_js/automailer");
-
-    /*var mail = {
-        // sender
-        from: 'SunlightCommunity18@gmail.com',
-        // subject
-        subject: 'Welcome to SunlightCommunity',
-        // receiver
-        to:'604271396@qq.com',//comes from front end
-        // content of the email;
-        text: 'This message comes from nodejs.'
-    };*/
-
-    //automailer(mail);
+    app.use('/paypal_start', paypalRouter);
 
 }
 module.exports=Load
