766651d692e94227d8b5cff965d5bb34f96a3f6f
Yixiang Wang <ywang597@sheffield.ac.uk>
2022-05-03 11:54:53 +0100

    qqq

--- project18/routes/PayPalResultSuccess.js
+++ project18/routes/PayPalResultSuccess.js
@@ -6,8 +6,8 @@ const paypal = require("./PayPalCfg");
 router.route("/")
     .get(function (req,res){
 
-        const payerId = req.body.PayerID;
-        const paymentId = req.body.paymentId;
+        const payerId = req.query.PayerID;
+        const paymentId = req.query.paymentId;
 
         const execute_payment_json = {
             "payer_id": payerId,
