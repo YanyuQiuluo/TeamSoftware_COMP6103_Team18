86d5fed207fd5c38acad04fdfd4fb184aac57f03
Yixiang Wang <ywang597@sheffield.ac.uk>
2022-05-04 13:37:01 +0100

    pay history

--- project18/app_WYX.js
+++ project18/app_WYX.js
@@ -3,11 +3,13 @@ var loginRouter = require('./routes/login');
 var countryDetail=require('./routes/CountryDetail');
 var success=require('./routes/PayPalResultSuccess');
 var cancel=require('./routes/PayPalResultCancel');
+var pay_history=require('./routes/PayHistory');
 function Load(app) {
     app.use('/login', loginRouter);
     app.use("/country_detail",countryDetail);
     app.use("/success",success);
     app.use("/cancel",cancel);
+    app.use("/pay_history",pay_history);
 }
 
 module.exports=Load
