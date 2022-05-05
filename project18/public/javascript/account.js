0c735d3dc0f5e50efe83be84bdb97d9b6e0f9b1e
Li Zeyu <zli259@sheffield.ac.uk>
2022-05-05 19:33:11 +0100

    Finish the function of accountPage
    We can get the data from mysql

--- project18/public/javascript/account.js
+++ project18/public/javascript/account.js
@@ -1,3 +1,4 @@
+let result = [];
 
 
 // let result = [] // response data array
@@ -43,31 +44,59 @@
         //     window.location.href='http://localhost:3000/loginPage'
         // }
 
-addData();
-function addData() {
-    // console.log("add data")
-    // console.log(document.getElementsByClassName("superuser"));
-    // document.getElementById('superuser').innerText = "qqq";
 
-
-
-    // var html = '';
-
-    // html += '<h1 class="index-header"> Welcome back' + '<h1>' + user2Name + '</h1>' + '</h1>'
-    // html += '<section class="centre-content bg-black ">\n' +
-    //     '<h1 class="index-header" > Welcome back <h1>' + user2Name + '</h1>\n' +
-    //     '</section>'
-    //
-    //
-    //
-    // $('.members-home').append(html);
-    // $(".members-home").trigger("create");
-}
 
 $(function(){
+    let object = window.sessionStorage.getItem("userName");
     let element = document.getElementById("scroll");
     if(element.scrollHeight <= element.clientHeight + 20) { /** There is a scroll bar, then fixed the foot bar*/
     document.getElementById("footer123").style.position = 'fixed'
     }
+
+    $.ajax({
+        type : 'POST',
+        url : 'http://localhost:3000/userPage',
+        dataType : 'json',
+        data : {
+            username : object
+        },
+        success : function (res){
+            if (res.code === '200'){
+                result = res.result
+                addData();
+                addTable();
+            } else {
+                alert('error')
+            }
+        }
+    });
+
+function addData(){
+    document.getElementById("uname").innerHTML = "Username: " + object
+    document.getElementById("emaiL").innerHTML = "Email: " + result[0].email
+    document.getElementById("carbonF").innerHTML = "Carbon footprint: " + result[0].feul_usage_pm
+    document.getElementById("Elect").innerHTML = "Electricity usage monthly: " + result[0].electicity_usage_pm
+
+
+}
+
+function addTable(){
+    var html = '';
+    for (var i = 0;i < result[1].length;i++){
+        html += '<tr>\n' +
+            '<td>' + result[1][i].user_name + '</td>\n' +
+            '<td>' + result[1][i].country_name + '</td>\n' +
+            '<td>' + result[1][i].transfer_amount + '</td>\n' +
+            '<td>' + result[1][i].dataTime + '</td>\n' +
+            '</tr>'
+    }
+    $('#tablePersonal').append(html);
+    $("#tablePersonal").trigger("create");
+}
+
 })
+
+
+
+
 
