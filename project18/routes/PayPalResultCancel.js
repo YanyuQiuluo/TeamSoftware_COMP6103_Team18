1ced8509bb88f50f1a9b849dfe0d29c7c2ec4cf0
Yue Zhou <yzhou209@sheffield.ac.uk>
2022-05-05 19:52:14 +0100

    status changes after payment finished

--- project18/routes/PayPalResultCancel.js
+++ project18/routes/PayPalResultCancel.js
@@ -3,10 +3,19 @@ const router = express.Router();
 const result = require("../mode_js/result");
 const Tokens = require("../mode_js/Tokens");
 const paypal = require("./PayPalCfg");
+const transaction = require("../Model/transaction");
 
 router.route("/")
-    .get(function (req,res){
+    .get(async function (req, res) {
         res.end("payment cancel");
+
+        const uuid = req.query.uuid;
+        const transaction = require("../Model/transaction");
+        await transaction.update({status: "cancel"}, {
+            where: {
+                uuid: uuid
+            }
+        });
     });
 
 module.exports=router;
