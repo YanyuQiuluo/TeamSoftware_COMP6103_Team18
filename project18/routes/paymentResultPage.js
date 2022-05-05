9d6fb8d5d684878da2f9a57c18fd29e9c45c7585
Yifei Guo <yguo94@sheffield.ac.uk>
2022-05-05 22:17:18 +0100

    Recover the route page

--- project18/routes/paymentResultPage.js
+++ project18/routes/paymentResultPage.js
@@ -1,22 +1,7 @@
 var express = require('express');
-const result = require("../mode_js/result");
-const transaction = require("../Model/transaction");
 var router = express.Router();
-
 /* GET home page. */
-
-router.route("/").
-    post( function (req, res) {result.Run(req,res,func);});
-
-async  function func(req,res){
-    //return the transaction history by uuid
-    const uuid = req.body.uuid;
-    const transaction = require("../Model/transaction");
-    return transaction.findAll({
-        where: {
-            uuid: req.body.uuid
-        }
-    });
-}
-
+router.get('/', function(req, res, next) {
+    res.render('paymentResult', { title: 'paymentResult' });
+});
 module.exports = router;
