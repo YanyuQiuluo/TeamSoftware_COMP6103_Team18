21f51e41af5c3435c661af4107a9efd741f005bb
Yue Zhou <yzhou209@sheffield.ac.uk>
2022-05-05 19:46:21 +0100

    USD to GBP

--- project18/routes/paymentResultPage.js
+++ project18/routes/paymentResultPage.js
@@ -9,7 +9,7 @@ router.route("/").
     post( function (req, res) {result.Run(req,res,func);});
 
 async  function func(req,res){
-    //根据uuid返回记录
+    //return the transaction history by uuid
     const uuid = req.body.uuid;
     const transaction = require("../Model/transaction");
     return transaction.findAll({
