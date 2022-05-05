d9b2dfb8e95ac4a13fcf44962e8aa987816b9e50
Yixiang Wang <ywang597@sheffield.ac.uk>
2022-05-05 17:05:27 +0100

    Update app_ZY.js

--- project18/app_ZY.js
+++ project18/app_ZY.js
@@ -1,5 +1,4 @@
-const registerRouter = require("./routes/register");
-const verifyRouter = require("./routes/register");
+const registerRouter = require("./routes/register_");
 const countrylistRouter = require("./routes/countrylist");
 const userPageRouter = require("./routes/userPage");
 const paypalRouter = require("./routes/paypal_start");
@@ -7,11 +6,9 @@ const paypalRouter = require("./routes/paypal_start");
 const automailer = require("./mode_js/automailer");
 const countrylistRouter_ = require("./routes/countrylist_");
 const userPageRouter_ = require("./routes/userPage_");
-const verifyRouter_ = require("./routes/register_");
 
 function Load(app) {
     app.use('/register', registerRouter);
-    app.use('/register', verifyRouter);
     app.use('/countrylist', countrylistRouter_);
     app.use('/userPage', userPageRouter_);
     app.use('/paypal_start', paypalRouter);
