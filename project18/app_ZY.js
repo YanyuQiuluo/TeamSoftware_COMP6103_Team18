9e2243916a7465b08481f089faf26fae9be0d106
Yue Zhou <yzhou209@sheffield.ac.uk>
2022-05-05 19:25:43 +0100

    update country detail and resultpage

--- project18/app_ZY.js
+++ project18/app_ZY.js
@@ -2,12 +2,14 @@ const registerRouter = require("./routes/register_");
 const paypalRouter = require("./routes/paypal_start");
 const countrylistRouter_ = require("./routes/countrylist_");
 const userPageRouter_ = require("./routes/userPage_");
+const paymentResultPageRouter = require("./routes/paymentResultPage");
 
 function Load(app) {
     app.use('/register', registerRouter);
     app.use('/paypal_start', paypalRouter);
     app.use('/countrylist', countrylistRouter_);
     app.use('/userPage', userPageRouter_);
+    app.use('/paymentResultPage', paymentResultPageRouter);
 
 }
 module.exports=Load
