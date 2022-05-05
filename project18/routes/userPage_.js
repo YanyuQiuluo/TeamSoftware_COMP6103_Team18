71fa3be617f612e55f329cb4814822112018b718
Yixiang Wang <ywang597@sheffield.ac.uk>
2022-05-05 17:55:53 +0100

    Update userPage_.js

--- project18/routes/userPage_.js
+++ project18/routes/userPage_.js
@@ -2,16 +2,17 @@ var express = require('express');
 const result = require("../mode_js/result");
 var router = express.Router();
 const m = require("../Model/User_detail");
-
+const  m2=require("../Model/transaction");
 router.route("/")
     .post( function (req, res) {result.Run(req,res,func);});
 
 async  function func(req,res){
-    let userid = req.body.email;
-    if(!userid)throw "userid null";
-    let re= await m.findOne({where:{"user_id":userid}})
+    let username = req.body.username;
+    if(!username)throw "username null";
+    let re= await m.findOne({where:{"username":username}})
     if(!re)throw "userid no found";
-    return re;
+    let re2=await  m2.findAll({where:{"user_name":username}})
+    return [re,re2];
 }
 
 module.exports = router;
