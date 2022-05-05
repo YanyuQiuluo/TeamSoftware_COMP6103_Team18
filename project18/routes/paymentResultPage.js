af60643c0acd0831764028e202c9db064f88c2a5
Yue Zhou <yzhou209@sheffield.ac.uk>
2022-05-05 19:25:43 +0100

    update country detail and resultpage

--- project18/routes/paymentResultPage.js
+++ project18/routes/paymentResultPage.js
@@ -1,18 +1,22 @@
 var express = require('express');
 const result = require("../mode_js/result");
+const transaction = require("../Model/transaction");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('paymentResult', { title: 'paymentResult' });
-});
 
 router.route("/").
     post( function (req, res) {result.Run(req,res,func);});
 
 async  function func(req,res){
-
-
+    //根据uuid返回记录
+    const uuid = req.body.uuid;
+    const transaction = require("../Model/transaction");
+    return transaction.findAll({
+        where: {
+            uuid: req.body.uuid
+        }
+    });
 }
 
 module.exports = router;
