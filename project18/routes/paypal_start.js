542cf03c5a6c33c611eccf59e9fe7cbb65059224
Yifei Guo <yguo94@sheffield.ac.uk>
2022-05-06 23:47:53 +0100

    Sync with the changes of interfaces

--- project18/routes/paypal_start.js
+++ project18/routes/paypal_start.js
@@ -74,9 +74,12 @@ router.post('/', async (req, res) => {
        // payment started record
        const transaction = require("../Model/transaction");
        for (var i = 0; i < req.body.basket.length; i++) {
-           // console.log(req.body.basket[i]);
-           let to = req.body.basket[i];
-            // to=JSON.parse(req.body.basket[i]);
+           let to = ''
+           if (typeof req.body.basket[i] == 'string') {
+               to=JSON.parse(req.body.basket[i]);
+           } else if (typeof req.body.basket[i] == 'object' && req.body.basket[i]) {
+               to = req.body.basket[i];
+           }
            console.log(to);
            const payment = await transaction.create({
                dataTime: getCurrentTime(),
