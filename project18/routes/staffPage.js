8a6ef90cb4598cb2796521c181cbc302d69b2743
Yue Zhou <yzhou209@sheffield.ac.uk>
2022-05-07 16:52:38 +0100

    update sql

--- project18/routes/staffPage.js
+++ project18/routes/staffPage.js
@@ -2,63 +2,12 @@ var express = require('express');
 const result = require("../mode_js/result");
 var router = express.Router();
 const Contry = require("../Model/Contry");
+const country_name = require("../Model/Contry_name");
 const transaction = require("../Model/transaction");
+//const {where} = require("sequelize/types");
 
 router.get('/', function(req, res, next) {
     res.render('staffPage', { title: 'staffPage' });
 });
-
-router.route("/")
-    .post(async function (req, res) {
-        // function of a staff:
-        //1. browsing countrylist.
-        //2. browsing transactionlist.
-        //3. browsing data calculated by upper two list.
-
-        let get_country = req.body.get_country;
-        let Countrylist = await Contry.findAll();
-
-
-
-
-
-        let returnlist = Countrylist;
-
-        for (let i = 0; i  < Countrylist.length; i++){
-
-            let country_name = Countrylist[i].get('country_name');
-            console.log(country_name);
-            //算出一个国家的所有值的和
-            returnlist[i] = await transaction.sum(
-                'transfer_amount',
-                {
-                    where:
-                        {"country_name": country_name
-                        }
-                });
-
-            }
-        res.json(result.success(returnlist));
-
-
-
-
-
-
-
-
-        // require("../mode_js/MongoDB")
-        //     .table("transaction",closeDB={})
-        //     .then(x=>{
-        //         transactionlist = x.findAll();
-        //     })
-        //     .then(x=>{
-        //         res.json(result.success(x));
-        //     })
-        //     //-----------------------------------------------------------------------------------------------------------------------------------------------
-        //     .catch(x=>res.json(result.fail(x)))
-        //     .finally(()=>{closeDB.invoke()})
-    });
-
 
 module.exports = router;
