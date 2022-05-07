74ff468ec23c1c93d6852052f3d31c4c2d67c88a
Yue Zhou <yzhou209@sheffield.ac.uk>
2022-05-07 10:50:39 +0100

    staffPage demo

---
+++ project18/public/javascript/staffPage.js
@@ -0,0 +1,83 @@
+let result = [];
+function get_country(i){
+    $.ajax({
+        type : 'POST',
+        url : 'http://localhost:3000/edit_country',
+        dataType : 'json',
+        data : {
+            get_country : i
+        },
+        success : function (res){
+            if (res.code === '200'){
+                result = res.result
+                console.log(result);
+                fresh();
+            } else {
+                alert('error')
+            }
+        }
+    });
+}
+function  fresh(){
+    let object = window.sessionStorage.getItem("userName");
+    let element = document.getElementById("scroll");
+    if(element.scrollHeight <= element.clientHeight + 20) { /** There is a scroll bar, then fixed the foot bar*/
+    document.getElementById("footer123").style.position = 'fixed'
+    }
+
+    $.ajax({
+        type : 'POST',
+        url : 'http://localhost:3000/user_list',
+        dataType : 'json',
+        data : {
+            username : object
+        },
+        success : function (res){
+            if (res.code === '200'){
+                result = res.result
+
+                // addData();
+                addTable();
+            } else {
+                alert('error')
+            }
+        }
+    });
+}
+
+$(fresh());
+
+// function addData(){
+//     document.getElementById("uname").innerHTML = "Username: " + object
+//     document.getElementById("emaiL").innerHTML = "Email: " + result[0].email
+//     document.getElementById("carbonF").innerHTML = "Carbon footprint: " + result[0].feul_usage_pm
+//     document.getElementById("Elect").innerHTML = "Electricity usage monthly: " + result[0].electicity_usage_pm
+//
+//
+// }
+
+function addTable(){
+
+    let html = '';
+    for (let i = 0;i < result.length;i++){
+        let func='\''+'upgrade('+result[i].user_id+')'+'\'';
+        console.log(func);
+        html += '<tr>\n' +
+            '<td>' + result[i].country + '</td>\n' +
+            '<td>' + result[i].transfer_Amount + '</td>\n' +
+            '<td>' + result[i].carbon_emission + '</td>\n' +
+            '<td>' + result[i].panels_has_received + '</td>\n' ;
+        // if(result[i].user_type=="household"){
+        //     html+='<td><button  style="width: 200px; height: 30px;"  onClick='+func+'> upgrade</button></td>\n';
+        // }
+        html+='</tr>'
+    }
+    document.getElementById("tablePersonal").innerHTML=html;
+    // $('#tablePersonal').append(html);
+    // $("#tablePersonal").trigger("create");
+}
+
+
+
+
+
--- project18/routes/staffPage.js
+++ project18/routes/staffPage.js
@@ -1,33 +1,51 @@
 var express = require('express');
 const result = require("../mode_js/result");
 var router = express.Router();
+const Contry = require("../Model/Contry");
+const transaction = require("../Model/transaction");
 
 router.get('/', function(req, res, next) {
     res.render('staffPage', { title: 'staffPage' });
 });
 
 router.route("/")
-    .post(function (req,res){
+    .post(async function (req, res) {
         // function of a staff:
         //1. browsing countrylist.
         //2. browsing transactionlist.
         //3. browsing data calculated by upper two list.
 
-        //require feature index
-        let userid = req.body.email;
-        let transactionlist;
+        let get_country = req.body.get_country;
+        let Countrylist = await Contry.findAll();
+        //let transactionlist = await transaction.findAll();
+        let returnlist;
 
-        require("../mode_js/MongoDB")
-            .table("transaction",closeDB={})
-            .then(x=>{
-                transactionlist = x.findAll();
-            })
-            .then(x=>{
-                res.json(result.success(x));
-            })
-            //-----------------------------------------------------------------------------------------------------------------------------------------------
-            .catch(x=>res.json(result.fail(x)))
-            .finally(()=>{closeDB.invoke()})
+        for (let i = 0; i  < Countrylist.length; i++){
+            console.log(Countrylist[i]);
+            res.json(Countrylist[i]);
+            //let countryname = Countrylist[i].get(country_name);
+            //console.log(countryname);
+
+            }
+
+
+
+
+
+
+
+
+        // require("../mode_js/MongoDB")
+        //     .table("transaction",closeDB={})
+        //     .then(x=>{
+        //         transactionlist = x.findAll();
+        //     })
+        //     .then(x=>{
+        //         res.json(result.success(x));
+        //     })
+        //     //-----------------------------------------------------------------------------------------------------------------------------------------------
+        //     .catch(x=>res.json(result.fail(x)))
+        //     .finally(()=>{closeDB.invoke()})
     });
 
 
