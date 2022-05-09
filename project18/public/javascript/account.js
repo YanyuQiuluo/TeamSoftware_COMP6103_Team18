4c8988e2082a6056e31917929ca4fedaee601154
Li Zeyu <zli259@sheffield.ac.uk>
2022-05-09 18:30:20 +0100

    add the new function about "Carbon footprint savings (kg) "

--- project18/public/javascript/account.js
+++ project18/public/javascript/account.js
@@ -1,52 +1,10 @@
-let result = [];
-
-
-// let result = [] // response data array
-
-// $(function(){
-//     let user2Name = window.sessionStorage.getItem("userName");
-//     $.ajax({
-//
-//         type: 'POST',
-//         url: 'http://localhost:3000/login',
-//         dataType: 'json',
-//         data: {
-//             userName: user2Name
-//         },
-//         success: function (res){
-//             if(res.code === '200'){
-//                 result = res.result;
-//                 addData();
-//             } else {
-//                 alert('error')
-//
-//             }
-//         }
-//
-//     });
-// })
-//
-// function addData(){
-//     var html = '';
-//     html += 'userName' + 'user2Name' +'123'
-//
-//     $('#user01').append(html).trigger( "create" );
-// }
-
-
-
-
-        // if (window.sessionStorage.getItem("userName")) {
-        //     document.getElementById("user01").innerHTML = window.sessionStorage.getItem("userName");
-        //
-        //
-        // } else {
-        //     window.location.href='http://localhost:3000/loginPage'
-        // }
-
-
+let result ;
+let electricity = [];
+let factor ; /** Array of price of solar panels */
 
 $(function(){
+
+
 
     if (!window.sessionStorage.getItem("userName")){
         window.location.replace("http://localhost:3000/loginPage");
@@ -58,6 +16,26 @@ $(function(){
     document.getElementById("footer123").style.position = 'fixed'
     }
 
+
+    // get the carbon_saving_factor
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:3000/countrylist',
+        dataType : 'json',
+        data: {
+            feature_index: 1,
+            ranking_index: 0
+        },
+        success: function(res){
+            if (res.code === '200') {
+                factor = res.result;
+               if(result) getSavingFootprint();
+                console.log("123");
+            } else {
+                alert(res.msg)
+            }
+        }
+    });
 
 
     $.ajax({
@@ -71,25 +49,76 @@ $(function(){
             if (res.code === '200'){
                 result = res.result
                 addData();
-                addTable();
+                if(factor) getSavingFootprint();
+
             } else {
                 alert(res.msg)
             }
         }
     });
 
+// carbon_saving_factor * panel_amout
+//
+function getSavingFootprint(){
+    //get the countryid
+    let toSaveF = 0;
+    let savingF = 0;
+    let cid;
+    console.log('getSavingFootprint')
+    console.log(result);
+    console.log(factor);
+    ff={}
+    factor.forEach(f=>{
+        ff[f.country_id]=f.carbon_saving_factor;
+    })
+   let total=0;
+    result[1].forEach(r=>{
+       let id= r.country_id;
+       let carbon_saving_factor=ff[id];
+       let amout = r.panel_amount;
+       let savingF = carbon_saving_factor*amout;
+       r.savingF=savingF;
+       // var html = '';
+       // html += '<td >' + savingF + '</td>'
+       //  $('#sss').after(html);
+       //  $("#sss").trigger("create");
+
+
+       if (savingF)total+=savingF;
+    })
+
+
+    addTable();
+
+    // for (let i = 0; i < result[1].length; i++) {
+    //     cid = result[1][i].country_id;
+    //     for (let j = 0; j < factor.length; j++) {
+    //         if (factor[i].country_id == cid) {
+    //             savingF = result[1].panel_amount * factor[i].carbon_saving_factor;
+    //         }
+    //     }
+    //     toSaveF += savingF;
+    // }
+    console.log("test")
+    document.getElementById("carbonS").innerHTML = "Carbon footprint savings (kg) : " + total ;
+}
+
+
+
 function addData(){
     document.getElementById("uname").innerHTML = "Username: " + result[0].username;
     document.getElementById("emaiL").innerHTML = "Email: " + result[0].email;
     // document.getElementById("carbonF").innerHTML = "Carbon footprint: " + result[0].feul_usage_pm
+    document.getElementById("carbonS").innerHTML = "Carbon footprint savings (kg) : 0"
     document.getElementById("carbonF").innerHTML = "Carbon footprint (Kg per Month) : 0"
+
     // document.getElementById("Elect").innerHTML = "Electricity usage monthly: " + result[0].electicity_usage_pm
 
 
 }
 
 function changeData(){
-    document.getElementById("carbonF").innerHTML =  "Carbon footprint (Kg per Month) : " + result;
+    document.getElementById("carbonF").innerHTML =  "Carbon footprint (Kg per Month) : " + electricity;
 }
 
 function addTable(){
@@ -98,13 +127,16 @@ function addTable(){
         html += '<tr>\n' +
             // '<td>' + result[1][i].user_name + '</td>\n' +
             '<td>' + result[1][i].country.country_name + '</td>\n' +
-            '<td>$' + result[1][i].transfer_amount + '</td>\n' +
+            '<td id="sss">￡' + result[1][i].transfer_amount + '</td>\n' +
+            '<td>' + result[1][i].savingF.toFixed(2) + 'kg</td>\n' +
             '<td>' + result[1][i].dataTime + '</td>\n' +
             '</tr>'
     }
     $('#tablePersonal').append(html);
     $("#tablePersonal").trigger("create");
 }
+
+
 
 
 
@@ -130,6 +162,8 @@ $('#submitEle').on('click',function (){
     //     // document.getElementById("carbonF").innerHTML = "Carbon footprint (Kg per Month) : wrong"
     // }
 
+
+
     $.ajax({
         type : 'POST',
         url : 'http://localhost:3000/getFootPrint',
@@ -142,7 +176,7 @@ $('#submitEle').on('click',function (){
             if (res.code === '200'){
 
                 console.log(res.result);
-                result = res.result;
+                electricity = res.result;
                 changeData();
             }else{
                 alert(res.msg)
@@ -152,7 +186,13 @@ $('#submitEle').on('click',function (){
     })
 
 
+
+
 })
+
+
+
+
 
 
 })
